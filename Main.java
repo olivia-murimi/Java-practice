867571ab8a870259df54a731ba90f2d4c81f8405
Olivia Waithira <oliviawaithira7@gmail.com>
2026-02-20 12:46:48 +0300

    variables

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int age =30;
-        int temperature = 20;
-        age = 35;
-        System.out.println(age);
-        System.out.println(temperature);
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+        
     }
 }
